3d86a2f1cd10c069996fc79dab1a680bf23e3422
Caleb Parrra <mccalebyt@gmail.com>
2022-06-30 09:53:56 -0500

    db conection and dbmodel

---
+++ backend/models/BlogModel.js
@@ -0,0 +1,10 @@
+//import db
+import db from "../database/db";
+import { DataTypes } from "sequelize/types";
+
+const BlogModel = db.define('blogs', {
+    title: {type: DataTypes.STRING},
+    content: {type: DataTypes.STRING}
+})
+
+export default BlogModel
